e99e8572f30302f225368de906edc5c4ecf6d729
Tetragon <sushilovvladislav@gmail.com>
2025-12-16 19:52:33 +0300

    New project added

---
+++ TriangleTypeCheck/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TriangleTypeCheck
+{
+    partial class TriangleTypeCheck
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBoxSizeA = new System.Windows.Forms.TextBox();
+            this.tBoxSizeB = new System.Windows.Forms.TextBox();
+            this.tBoxSizeC = new System.Windows.Forms.TextBox();
+            this.grpBoxInput = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSizeC = new System.Windows.Forms.Label();
+            this.lblSizeB = new System.Windows.Forms.Label();
+            this.lblSizeA = new System.Windows.Forms.Label();
+            this.pnlHead = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblHead = new System.Windows.Forms.Label();
+            this.pBoxRes = new System.Windows.Forms.PictureBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.grpBoxInput.SuspendLayout();
+            this.pnlHead.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxRes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBoxSizeA
+            // 
+            this.tBoxSizeA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBoxSizeA.Location = new System.Drawing.Point(128, 43);
+            this.tBoxSizeA.Name = "tBoxSizeA";
+            this.tBoxSizeA.Size = new System.Drawing.Size(186, 29);
+            this.tBoxSizeA.TabIndex = 0;
+            this.tBoxSizeA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tBoxSizeB
+            // 
+            this.tBoxSizeB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBoxSizeB.Location = new System.Drawing.Point(128, 77);
+            this.tBoxSizeB.Name = "tBoxSizeB";
+            this.tBoxSizeB.Size = new System.Drawing.Size(186, 29);
+            this.tBoxSizeB.TabIndex = 1;
+            this.tBoxSizeB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // tBoxSizeC
+            // 
+            this.tBoxSizeC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBoxSizeC.Location = new System.Drawing.Point(128, 112);
+            this.tBoxSizeC.Name = "tBoxSizeC";
+            this.tBoxSizeC.Size = new System.Drawing.Size(186, 29);
+            this.tBoxSizeC.TabIndex = 2;
+            this.tBoxSizeC.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // grpBoxInput
+            // 
+            this.grpBoxInput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.grpBoxInput.Controls.Add(this.button2);
+            this.grpBoxInput.Controls.Add(this.button1);
+            this.grpBoxInput.Controls.Add(this.lblSizeC);
+            this.grpBoxInput.Controls.Add(this.lblSizeB);
+            this.grpBoxInput.Controls.Add(this.lblSizeA);
+            this.grpBoxInput.Controls.Add(this.tBoxSizeA);
+            this.grpBoxInput.Controls.Add(this.tBoxSizeC);
+            this.grpBoxInput.Controls.Add(this.tBoxSizeB);
+            this.grpBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.grpBoxInput.Location = new System.Drawing.Point(12, 136);
+            this.grpBoxInput.Name = "grpBoxInput";
+            this.grpBoxInput.Size = new System.Drawing.Size(336, 239);
+            this.grpBoxInput.TabIndex = 3;
+            this.grpBoxInput.TabStop = false;
+            this.grpBoxInput.Text = "Входные данные";
+            this.grpBoxInput.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(169, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(10, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Определить";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // lblSizeC
+            // 
+            this.lblSizeC.AutoSize = true;
+            this.lblSizeC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSizeC.Location = new System.Drawing.Point(7, 115);
+            this.lblSizeC.Name = "lblSizeC";
+            this.lblSizeC.Size = new System.Drawing.Size(110, 24);
+            this.lblSizeC.TabIndex = 5;
+            this.lblSizeC.Text = "Сторона C:";
+            this.lblSizeC.Click += new System.EventHandler(this.lblSizeC_Click);
+            // 
+            // lblSizeB
+            // 
+            this.lblSizeB.AutoSize = true;
+            this.lblSizeB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSizeB.Location = new System.Drawing.Point(6, 80);
+            this.lblSizeB.Name = "lblSizeB";
+            this.lblSizeB.Size = new System.Drawing.Size(109, 24);
+            this.lblSizeB.TabIndex = 4;
+            this.lblSizeB.Text = "Сторона B:";
+            // 
+            // lblSizeA
+            // 
+            this.lblSizeA.AutoSize = true;
+            this.lblSizeA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSizeA.Location = new System.Drawing.Point(6, 46);
+            this.lblSizeA.Name = "lblSizeA";
+            this.lblSizeA.Size = new System.Drawing.Size(110, 24);
+            this.lblSizeA.TabIndex = 3;
+            this.lblSizeA.Text = "Сторона A:";
+            this.lblSizeA.Click += new System.EventHandler(this.lblSizeA_Click);
+            // 
+            // pnlHead
+            // 
+            this.pnlHead.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlHead.Controls.Add(this.btnExit);
+            this.pnlHead.Controls.Add(this.lblHead);
+            this.pnlHead.Location = new System.Drawing.Point(-2, -4);
+            this.pnlHead.Name = "pnlHead";
+            this.pnlHead.Size = new System.Drawing.Size(788, 105);
+            this.pnlHead.TabIndex = 3;
+            this.pnlHead.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.Window;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(616, 16);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(158, 70);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblHead
+            // 
+            this.lblHead.AutoSize = true;
+            this.lblHead.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblHead.Location = new System.Drawing.Point(7, 30);
+            this.lblHead.Name = "lblHead";
+            this.lblHead.Size = new System.Drawing.Size(593, 42);
+            this.lblHead.TabIndex = 0;
+            this.lblHead.Text = "Определение типа треугольника";
+            this.lblHead.Click += new System.EventHandler(this.lblHead_Click);
+            // 
+            // pBoxRes
+            // 
+            this.pBoxRes.Location = new System.Drawing.Point(418, 136);
+            this.pBoxRes.Name = "pBoxRes";
+            this.pBoxRes.Size = new System.Drawing.Size(306, 193);
+            this.pBoxRes.TabIndex = 4;
+            this.pBoxRes.TabStop = false;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblRes.Location = new System.Drawing.Point(413, 341);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(313, 25);
+            this.lblRes.TabIndex = 6;
+            this.lblRes.Text = "Равнобедренный треугольник";
+            this.lblRes.Click += new System.EventHandler(this.lblRes_Click);
+            // 
+            // TriangleTypeCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.pBoxRes);
+            this.Controls.Add(this.pnlHead);
+            this.Controls.Add(this.grpBoxInput);
+            this.Name = "TriangleTypeCheck";
+            this.Text = "Triangle Type Check";
+            this.Load += new System.EventHandler(this.TriangleTypeCheck_Load);
+            this.grpBoxInput.ResumeLayout(false);
+            this.grpBoxInput.PerformLayout();
+            this.pnlHead.ResumeLayout(false);
+            this.pnlHead.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxRes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxSizeA;
+        private System.Windows.Forms.TextBox tBoxSizeB;
+        private System.Windows.Forms.TextBox tBoxSizeC;
+        private System.Windows.Forms.GroupBox grpBoxInput;
+        private System.Windows.Forms.Panel pnlHead;
+        private System.Windows.Forms.Label lblSizeC;
+        private System.Windows.Forms.Label lblSizeB;
+        private System.Windows.Forms.Label lblSizeA;
+        private System.Windows.Forms.Label lblHead;
+        private System.Windows.Forms.PictureBox pBoxRes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ TriangleTypeCheck/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriangleTypeCheck
+{
+    public partial class TriangleTypeCheck : Form
+    {
+        public TriangleTypeCheck()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSizeC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSizeA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblHead_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TriangleTypeCheck_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblRes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
