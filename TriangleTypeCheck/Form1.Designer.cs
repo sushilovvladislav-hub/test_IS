10066f3a170f14661974d0d85f54e59776318eff
Tetragon <sushilovvladislav@gmail.com>
2025-12-17 00:10:20 +0300

    Some changes added

--- TriangleTypeCheck/Form1.Designer.cs
+++ TriangleTypeCheck/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TriangleTypeCheck));
             this.tBoxSizeA = new System.Windows.Forms.TextBox();
             this.tBoxSizeB = new System.Windows.Forms.TextBox();
             this.tBoxSizeC = new System.Windows.Forms.TextBox();
@@ -42,9 +43,11 @@
             this.lblHead = new System.Windows.Forms.Label();
             this.pBoxRes = new System.Windows.Forms.PictureBox();
             this.lblRes = new System.Windows.Forms.Label();
+            this.grBoxRes = new System.Windows.Forms.GroupBox();
             this.grpBoxInput.SuspendLayout();
             this.pnlHead.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxRes)).BeginInit();
+            this.grBoxRes.SuspendLayout();
             this.SuspendLayout();
             // 
             // tBoxSizeA
@@ -54,7 +57,6 @@
             this.tBoxSizeA.Name = "tBoxSizeA";
             this.tBoxSizeA.Size = new System.Drawing.Size(186, 29);
             this.tBoxSizeA.TabIndex = 0;
-            this.tBoxSizeA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // tBoxSizeB
             // 
@@ -63,7 +65,6 @@
             this.tBoxSizeB.Name = "tBoxSizeB";
             this.tBoxSizeB.Size = new System.Drawing.Size(186, 29);
             this.tBoxSizeB.TabIndex = 1;
-            this.tBoxSizeB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // tBoxSizeC
             // 
@@ -72,7 +73,6 @@
             this.tBoxSizeC.Name = "tBoxSizeC";
             this.tBoxSizeC.Size = new System.Drawing.Size(186, 29);
             this.tBoxSizeC.TabIndex = 2;
-            this.tBoxSizeC.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // grpBoxInput
             // 
@@ -92,7 +92,6 @@
             this.grpBoxInput.TabIndex = 3;
             this.grpBoxInput.TabStop = false;
             this.grpBoxInput.Text = "Входные данные";
-            this.grpBoxInput.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // button2
             // 
@@ -123,7 +122,6 @@
             this.lblSizeC.Size = new System.Drawing.Size(110, 24);
             this.lblSizeC.TabIndex = 5;
             this.lblSizeC.Text = "Сторона C:";
-            this.lblSizeC.Click += new System.EventHandler(this.lblSizeC_Click);
             // 
             // lblSizeB
             // 
@@ -144,7 +142,6 @@
             this.lblSizeA.Size = new System.Drawing.Size(110, 24);
             this.lblSizeA.TabIndex = 3;
             this.lblSizeA.Text = "Сторона A:";
-            this.lblSizeA.Click += new System.EventHandler(this.lblSizeA_Click);
             // 
             // pnlHead
             // 
@@ -155,7 +152,6 @@
             this.pnlHead.Name = "pnlHead";
             this.pnlHead.Size = new System.Drawing.Size(788, 105);
             this.pnlHead.TabIndex = 3;
-            this.pnlHead.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // btnExit
             // 
@@ -178,13 +174,14 @@
             this.lblHead.Size = new System.Drawing.Size(593, 42);
             this.lblHead.TabIndex = 0;
             this.lblHead.Text = "Определение типа треугольника";
-            this.lblHead.Click += new System.EventHandler(this.lblHead_Click);
             // 
             // pBoxRes
             // 
-            this.pBoxRes.Location = new System.Drawing.Point(418, 136);
+            this.pBoxRes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pBoxRes.ImageLocation = "";
+            this.pBoxRes.Location = new System.Drawing.Point(35, 30);
             this.pBoxRes.Name = "pBoxRes";
-            this.pBoxRes.Size = new System.Drawing.Size(306, 193);
+            this.pBoxRes.Size = new System.Drawing.Size(306, 182);
             this.pBoxRes.TabIndex = 4;
             this.pBoxRes.TabStop = false;
             // 
@@ -192,32 +189,44 @@
             // 
             this.lblRes.AutoSize = true;
             this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblRes.Location = new System.Drawing.Point(413, 341);
+            this.lblRes.Location = new System.Drawing.Point(30, 213);
             this.lblRes.Name = "lblRes";
-            this.lblRes.Size = new System.Drawing.Size(313, 25);
+            this.lblRes.Size = new System.Drawing.Size(204, 25);
             this.lblRes.TabIndex = 6;
-            this.lblRes.Text = "Равнобедренный треугольник";
-            this.lblRes.Click += new System.EventHandler(this.lblRes_Click);
+            this.lblRes.Text = "(Тип треугольника)";
+            // 
+            // grBoxRes
+            // 
+            this.grBoxRes.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.grBoxRes.Controls.Add(this.pBoxRes);
+            this.grBoxRes.Controls.Add(this.lblRes);
+            this.grBoxRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.grBoxRes.Location = new System.Drawing.Point(377, 136);
+            this.grBoxRes.Name = "grBoxRes";
+            this.grBoxRes.Size = new System.Drawing.Size(374, 241);
+            this.grBoxRes.TabIndex = 7;
+            this.grBoxRes.TabStop = false;
+            this.grBoxRes.Text = "Результат";
             // 
             // TriangleTypeCheck
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.lblRes);
-            this.Controls.Add(this.pBoxRes);
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.grBoxRes);
             this.Controls.Add(this.pnlHead);
             this.Controls.Add(this.grpBoxInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TriangleTypeCheck";
             this.Text = "Triangle Type Check";
-            this.Load += new System.EventHandler(this.TriangleTypeCheck_Load);
             this.grpBoxInput.ResumeLayout(false);
             this.grpBoxInput.PerformLayout();
             this.pnlHead.ResumeLayout(false);
             this.pnlHead.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxRes)).EndInit();
+            this.grBoxRes.ResumeLayout(false);
+            this.grBoxRes.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -237,6 +246,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.GroupBox grBoxRes;
     }
 }
 
--- TriangleTypeCheck/Form1.cs
+++ TriangleTypeCheck/Form1.cs
@@ -12,59 +12,10 @@ namespace TriangleTypeCheck
 {
     public partial class TriangleTypeCheck : Form
     {
+
         public TriangleTypeCheck()
         {
             InitializeComponent();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblSizeC_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblSizeA_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblHead_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TriangleTypeCheck_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblRes_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void btnExit_Click(object sender, EventArgs e)
